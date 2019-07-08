7dfcc20a5a456c0c834284b5d3dae784f1370b09
ParkSeoEun <silion10@naver.com>
2019-07-08 11:22:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/semiprojectsample/bean/MemoBean.java
@@ -0,0 +1,27 @@
+package com.example.semiprojectsample.bean;
+
+public class MemoBean {
+
+    private String memoPicPath;
+    private String memo;
+    private String memoDate;
+
+    public void setMemoPicPath(String memoPicPath) {
+        this.memoPicPath = memoPicPath;
+    }
+    public String getMemoPicPath() {
+        return memoPicPath;
+    }
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+    public String getMemo() {
+        return memo;
+    }
+    public void setMemoDate(String memoDate) {
+        this.memoDate = memoDate;
+    }
+    public String getMemoDate() {
+        return memoDate;
+    }
+}
